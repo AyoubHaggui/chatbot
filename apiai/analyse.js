da24a63499d2a718470d0ac9b8443c35d6f8223b
micro3thimin <ayoub.haggui@gmail.com>
2017-10-26 04:55:33 +0100

    Finally made a blocking function: analyse work

--- apiai/analyse.js
+++ apiai/analyse.js
@@ -2,16 +2,23 @@ apiai = require('apiai')
 const apiToken = require('../tokens').apiToken
 var app = apiai('8cac96b981e441f489cfabee2558319a')
 exports = module.exports = {}
+/**
+ * Uses apiai module to analyse input text
+ * @param  {string} sender Session id used to keep track of context in apiai
+ * @param  {string} text   text to analyse
+ */
 exports.analyse = function (sender, text){
-	console.log("api active")
 	var request = app.textRequest(text, {
-		sessionId: 123
+		sessionId: sender
 	})
-	request.on('response', function(response){
-		console.log(response)
-	})
-	request.on('error', function(error) {
-    console.log(error);
+	var response =  new Promise(function (resolve, reject){
+		request.on('response', function(response){
+			resolve(response)
+		})
+		request.on('error', function(error) {
+			reject(error)
+		})
 	})
 	request.end()
+	return(response)
 }
--- request/textMessage.js
+++ request/textMessage.js
@@ -4,13 +4,28 @@
 var ai = require('../apiai/analyse')
 var textResponse = require('../response/textResponse').textResponse
 exports = module.exports = {}
-exports.textMessage = function(messaging){
+/**
+ * Registers user and message to database & sends message for analysis
+ * @param  {JSON} messaging messaging event request
+ */
+exports.textMessage = async function(messaging){
 	console.log(messaging)
 	var sender= messaging.sender.id
 	var message=messaging.message.text
 	var timestamp = messaging.timestamp
-	console.log(message)
-	ai.analyse(sender, message)
+	try{
+		var response = await ai.analyse(sender ,message)
+	}
+	catch(e){
+		console.log(e)
+	}
+	if(response.result.actionIncomplete){
+		console.log("redirecting to intent")
+	}
+	else{
+		console.log(response.result.fulfillment.speech)
+	}
+	// console.log(response)
 	// response = analysis.next()
 	// if (parametersSet(response)){
 		// switch(response.intent){
